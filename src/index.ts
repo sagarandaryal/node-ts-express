efcb7d46f6ebc6d2ab4587667675ed140503d73c
sagar-aryal <sagar_yal92@outlook.com>
2022-05-19 00:35:01 +0300

    CRUD api created for products

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import productsRouter from "./routes/productsRoute";
 
 const app = express();
 // initialize configuration
@@ -9,6 +10,10 @@ dotenv.config();
 const hostname = process.env.SERVER_HOSTNAME;
 const port = process.env.SERVER_PORT;
 
+// application middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res
     .status(200)
@@ -16,6 +21,8 @@ app.get("/", (req, res) => {
       "Server is running in the backend after folder setup with env config."
     );
 });
+
+app.use("/", productsRouter);
 
 app.listen(port, () => {
   console.log(`Server has started at http://${hostname}:${port}.`);
---
+++ src/routes/productsRoute.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/products", (req, res) => {
+  res.status(200).send("Get request to the products page");
+});
+
+router.post("/products", (req, res) => {
+  res.status(200).send("Post request to the products page");
+});
+
+router.put("/products/:id", (req, res) => {
+  console.log(req.body, req.query);
+  res.status(200).send("Put request to the products page");
+});
+
+router.delete("/products/:id", (req, res) => {
+  console.log(req.params);
+  res.status(200).send("Delete request to the products page");
+});
+
+export default router;
