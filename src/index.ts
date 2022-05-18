670ffaa18308386b79c7a13bf14da244ce22c3fe
sagar-aryal <sagar_yal92@outlook.com>
2022-05-18 23:43:11 +0300

    env file hidden

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
-const hostname = "localhost";
-const port = 8000;
+// initialize configuration
+dotenv.config();
+
+// port is now available to the Node.js runtime as if it were an environment variable
+const hostname = process.env.SERVER_HOSTNAME;
+const port = process.env.SERVER_PORT;
 
 app.get("/", (req, res) => {
-  res.status(200).send("Server is running in the backend after folder setup.");
+  res
+    .status(200)
+    .send(
+      "Server is running in the backend after folder setup with env config."
+    );
 });
 
 app.listen(port, () => {
